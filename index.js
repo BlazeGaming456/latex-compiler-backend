d7d7bb3aa83166319ecbefd3df644a85d0f7469b
unknown <ajincraju@gmail.com>
2025-06-27 22:44:59 +0530

    Fixed some errors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const cors = require("cors");
 const { exec } = require("child_process");
 const path = require("path");
 
